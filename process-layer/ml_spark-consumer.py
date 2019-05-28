61ee78e370e9b0d7a8812c7eb8c14ae4aec8e57a
helioncneto <helioncneto@gmail.com>
2019-05-27 22:57:47 -0300

    Atualizando ml spark

--- process-layer/ml_spark-consumer.py
+++ process-layer/ml_spark-consumer.py
@@ -5,12 +5,10 @@
 #
 
 import findspark
-
-# findspark.init('/home/administrador/MineCap/infra/spark-2.3.0-bin-hadoop2.7')
+#findspark.init('/home/administrador/MineCap/infra/spark-2.3.0-bin-hadoop2.7')
 findspark.init('/opt/spark')
 
 import os
-
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.0 pyspark-shell'
 
 import sys
@@ -26,6 +24,7 @@ from pyspark.sql import Row
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml.feature import PCA
 from pyspark.ml.classification import RandomForestClassifier
+from sklearn import linear_model
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
@@ -39,36 +38,34 @@ sc = SparkContext(conf=conf)
 sc.setLogLevel("WARN")
 ssc = StreamingContext(sc, n_secs)
 
-spSession = SparkSession.builder.master("local").appName("MineCap").config("spark.some.config.option",
-                                                                           "some-value").getOrCreate()
+spSession = SparkSession.builder.master("local").appName("MineCap").config("spark.some.config.option", "some-value").getOrCreate()
 
 kafkaStream = KafkaUtils.createDirectStream(ssc, [topic], {
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'flow-streaming',
-    'fetch.message.max.bytes': '15728640',
-    'auto.offset.reset': 'largest'})
+    'bootstrap.servers':'localhost:9092',
+    'group.id':'flow-streaming',
+    'fetch.message.max.bytes':'15728640',
+    'auto.offset.reset':'largest'})
 # Group ID is completely arbitrary
 
 lines = kafkaStream.map(lambda x: x[1])
-flows = lines.flatMap(lambda line: line.split(" "))  # .map(lambda word: (word[1:-1].split(",")))
+flows = lines.flatMap(lambda line: line.split(" "))#.map(lambda word: (word[1:-1].split(",")))
 
 ##### tratamento dos dados
 
-# fluxoRDD = sc.textFile("/home/administrador/MineCap/process-layer/dataset_fluxo_bc.csv")
+#fluxoRDD = sc.textFile("/home/administrador/MineCap/process-layer/dataset_fluxo_bc.csv")
 fluxoRDD = sc.textFile("/home/helio/MineCap/process-layer/dataset_novo.csv")
 
 # Removendo a primeira linha do arquivo (cabeçalho)
 firstLine = fluxoRDD.first()
 fluxoRDD2 = fluxoRDD.filter(lambda x: x != firstLine)
 
-
-def transformToNumeric(inputStr):
+def transformToNumeric(inputStr) :
     attList = inputStr.split(",")
-    # srcip = float(attList[0])
-    # srcport = float(attList[1])
-    # dstip = float(attList[2])
-    # dstport = float(attList[3])
-    # proto = 1.0 if attList[4] == "tcp" else 0.0
+    #srcip = float(attList[0])
+    #srcport = float(attList[1])
+    #dstip = float(attList[2])
+    #dstport = float(attList[3])
+    #proto = 1.0 if attList[4] == "tcp" else 0.0
     total_fpackets = float(attList[5])
     total_fvolume = float(attList[6])
     total_bpackets = float(attList[7])
@@ -104,8 +101,8 @@ def transformToNumeric(inputStr):
     sflow_bbytes = float(attList[37])
     fpsh_cnt = float(attList[38])
     bpsh_cnt = float(attList[39])
-    # furg_cnt = float(attList[40])
-    # burg_cnt = float(attList[41])
+    #furg_cnt = float(attList[40])
+    #burg_cnt = float(attList[41])
     total_fhlen = float(attList[42])
     total_bhlen = float(attList[43])
     dscp = float(attList[44])
@@ -126,8 +123,7 @@ def transformToNumeric(inputStr):
 
     return linhas
 
-
-def transformToNumeric2(inputStr):
+def transformToNumeric2(inputStr) :
     attList = inputStr.split(",")
     srcip = str(attList[0])
     srcport = int(attList[1])
@@ -169,37 +165,34 @@ def transformToNumeric2(inputStr):
     sflow_bbytes = float(attList[37])
     fpsh_cnt = float(attList[38])
     bpsh_cnt = float(attList[39])
-    # furg_cnt = float(attList[40])
-    # burg_cnt = float(attList[41])
+    #furg_cnt = float(attList[40])
+    #burg_cnt = float(attList[41])
     total_fhlen = float(attList[42])
     total_bhlen = float(attList[43])
     dscp = float(attList[44])
     classe = 0.0
-    # Cria as linhas com os objetos transformados
-    linhas = Row(classe=classe, srcip=srcip, srcport=srcport, dstip=dstip, dstport=dstport, proto=proto,
-                 total_fpackets=total_fpackets, total_fvolume=total_fvolume, total_bpackets=total_bpackets,
-                 total_bvolume=total_bvolume, min_fpktl=min_fpktl, mean_fpktl=mean_fpktl, max_fpktl=max_fpktl,
-                 std_fpktl=std_fpktl, min_bpktl=min_bpktl, mean_bpktl=mean_bpktl, max_bpktl=max_bpktl,
-                 std_bpktl=std_bpktl,
-                 min_fiat=min_fiat, mean_fiat=mean_fiat, max_fiat=max_fiat, std_fiat=std_fiat, min_biat=min_biat,
-                 mean_biat=mean_biat, max_biat=max_biat, std_biat=std_biat, duration=duration,
-                 min_active=min_active, mean_active=mean_active, max_active=max_active,
-                 std_active=std_active, min_idle=min_idle, mean_idle=mean_idle, max_idle=max_idle,
-                 std_idle=std_idle, sflow_fpackets=sflow_fpackets, sflow_fbytes=sflow_fbytes,
-                 sflow_bpackets=sflow_bpackets, sflow_bbytes=sflow_bbytes, fpsh_cnt=fpsh_cnt,
-                 bpsh_cnt=bpsh_cnt, total_fhlen=total_fhlen,
-                 total_bhlen=total_bhlen, dscp=dscp)
+    #Cria as linhas com os objetos transformados
+    linhas = Row(classe = classe, srcip = srcip, srcport = srcport, dstip = dstip, dstport = dstport, proto = proto,
+                 total_fpackets = total_fpackets, total_fvolume = total_fvolume,total_bpackets = total_bpackets,
+                 total_bvolume = total_bvolume, min_fpktl = min_fpktl, mean_fpktl = mean_fpktl, max_fpktl = max_fpktl,
+                 std_fpktl = std_fpktl, min_bpktl = min_bpktl, mean_bpktl = mean_bpktl, max_bpktl = max_bpktl, std_bpktl = std_bpktl,
+                 min_fiat = min_fiat, mean_fiat = mean_fiat, max_fiat = max_fiat, std_fiat = std_fiat, min_biat = min_biat,
+                 mean_biat = mean_biat, max_biat = max_biat, std_biat = std_biat, duration = duration,
+                 min_active = min_active, mean_active = mean_active, max_active = max_active,
+                 std_active = std_active, min_idle = min_idle, mean_idle = mean_idle, max_idle = max_idle,
+                 std_idle = std_idle, sflow_fpackets = sflow_fpackets, sflow_fbytes = sflow_fbytes,
+                 sflow_bpackets = sflow_bpackets, sflow_bbytes = sflow_bbytes, fpsh_cnt = fpsh_cnt,
+                 bpsh_cnt = bpsh_cnt, total_fhlen = total_fhlen,
+                 total_bhlen = total_bhlen, dscp = dscp)
 
     return linhas
-
 
 fluxoRDD3 = fluxoRDD2.map(transformToNumeric)
 
 # Transforma para Dataframe
 fluxoDF = spSession.createDataFrame(fluxoRDD3)
 
-
-def transformaVar(row):
+def transformaVar(row) :
     obj = (row["classe"], Vectors.dense([row["total_fpackets"], row["total_fvolume"], row["total_bpackets"],
                                          row["total_bvolume"], row["min_fpktl"], row["mean_fpktl"],
                                          row["max_fpktl"], row["std_fpktl"], row["min_bpktl"], row["mean_bpktl"],
@@ -211,11 +204,9 @@ def transformaVar(row):
                                          row["sflow_fbytes"], row["sflow_bpackets"], row["sflow_bbytes"],
                                          row["fpsh_cnt"], row["bpsh_cnt"],
                                          row["total_fhlen"], row["total_bhlen"], row["dscp"]]))
-
     return obj
 
-
-def transformaVar2(row):
+def transformaVar2(row) :
     obj = (row["classe"], Vectors.dense([row["total_fpackets"], row["total_fvolume"], row["total_bpackets"],
                                          row["total_bvolume"], row["min_fpktl"], row["mean_fpktl"],
                                          row["max_fpktl"], row["std_fpktl"], row["min_bpktl"], row["mean_bpktl"],
@@ -229,31 +220,65 @@ def transformaVar2(row):
                                          row["total_fhlen"], row["total_bhlen"], row["dscp"]]))
     return obj
 
-
 fluxoRDD4 = fluxoDF.rdd.map(transformaVar)
 
-fluxoDF = spSession.createDataFrame(fluxoRDD4, ["rotulo", "atributos"])
+fluxoDF = spSession.createDataFrame(fluxoRDD4,["rotulo", "atributos"])
 
 scaler = MinMaxScaler(inputCol="atributos", outputCol="scaledFeatures", min=0.0, max=1.0)
 scalerModel = scaler.fit(fluxoDF)
 scaledData = scalerModel.transform(fluxoDF)
 
 # Indexação é pré-requisito para Decision Trees
-stringIndexer = StringIndexer(inputCol="rotulo", outputCol="indexed")
+stringIndexer = StringIndexer(inputCol = "rotulo", outputCol = "indexed")
 si_model = stringIndexer.fit(scaledData)
 obj_final = si_model.transform(scaledData)
 
 # Criando o modelo
-rfClassifer = RandomForestClassifier(labelCol="indexed", featuresCol="scaledFeatures", probabilityCol="probability",
-                                     numTrees=20)
+rfClassifer = RandomForestClassifier(labelCol = "indexed", featuresCol = "scaledFeatures", probabilityCol = "probability", numTrees=20)
 modelo = rfClassifer.fit(obj_final)
 
+'''
+def gravar_arq(L):
+    with open(L[0], 'a') as arq:
+        if len(L)==2:
+            arq.write(str(L[1]))
+            arq.write('\n')
+        else:
+            arq.write(str(L[1]))
+            arq.write(',')
+            arq.write(str(L[2]))
+            arq.write('\n')
+
+    arq.close()
+'''
+'''
+def gravar_arq1(L):
+    ln1, ln2, ln3, ln4 = L[0],L[1],L[2],L[3]
+    print('oi')
+    with open('results.txt', 'a') as arq:
+        arq.write(str(ln1))
+        arq.write(',')
+        arq.write(str(ln2))
+        arq.write('\n')
+    arq.close()
+    with open('outputs.txt', 'a') as arq:
+        arq.write(str(ln2))
+        arq.write('\n')
+    arq.close()
+    with open('fluxo_puro.txt', 'a') as arq:
+        arq.write(str(ln3))
+        arq.write('\n')
+    arq.close()
+    with open('proba.txt', 'a') as arq:
+        arq.write(str(ln4))
+        arq.write('\n')
+    arq.close()
+    if ln2 == 1:
+        add_flow(ln1[0], ln1[2])
+        add_flow(ln1[2], ln1[0])
+'''
 
 def output_rdd(rdd):
-    output = []
-    fluxo = []
-    s_classe = []
-    probability = []
 
     if not rdd.isEmpty():
         rdd2 = rdd.map(transformToNumeric2)
@@ -265,25 +290,15 @@ def output_rdd(rdd):
         string_Indexer = StringIndexer(inputCol="rotulo", outputCol="indexed")
         si__model = stringIndexer.fit(scaled_Data)
         obj__final = si__model.transform(scaled_Data)
-        # print(obj__final.select("scaledFeatures").show(10))
+        #print(obj__final.select("scaledFeatures").show(10))
         predictions = modelo.transform(obj__final)
-        # print(predictions.select("prediction", "scaledFeatures").show(10))
-        for i in predictions.select("prediction").collect():
-            output.append(i["prediction"])
+        #print(predictions.select("prediction", "scaledFeatures").show(10))
 
-        for i in rdd2.collect():
-            fluxo.append([i["srcip"][1:], i["srcport"], i["dstip"], i["dstport"]])
-
-        for i in predictions.select("probability").collect():
-            probability.append(i["probability"])
-
-        for i in rdd.collect():
-            s_classe.append(i)
-
-        # evaluator = MulticlassClassificationEvaluator(predictionCol = "prediction", labelCol = "indexed", metricName = "accuracy")
-        # print(evaluator.evaluate(predictions))
-
-        # print(predictions.groupBy("indexed", "prediction").count().show())
+        output = map(lambda pred: pred['prediction'], predictions.select("prediction").collect())
+        fluxo = map(lambda fl: [fl["srcip"][1:], fl["srcport"], fl["dstip"], fl["dstport"]], rdd2.collect())
+        probability = map(lambda prob: prob["probability"], predictions.select("probability").collect())
+        s_classe = map(lambda fp: fp, rdd.collect())
+        #map(gravar_arq, [fluxo, output, s_classe, probability])
 
         for ln1, ln2, ln3, ln4 in zip(fluxo, output, s_classe, probability):
             with open('results.txt', 'a') as arq:
@@ -304,10 +319,8 @@ def output_rdd(rdd):
                 add_flow(ln1[0], ln1[2])
                 add_flow(ln1[2], ln1[0])
 
-
 flows.foreachRDD(lambda rdd: output_rdd(rdd))
 
 # Inicia o streaming
 ssc.start()
 ssc.awaitTermination()
-
