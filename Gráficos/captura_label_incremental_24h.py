48a024df0dbad40e20a3913c3e486a48782fe85d
Helio N. Cunha Neto <helioncneto@gmail.com>
2019-10-28 15:46:10 -0300

    Programa arquivo

---
+++ Gráficos/captura_label_incremental_24h.py
@@ -0,0 +1,54 @@
+def get_label(min, periodo):
+    for p in range(1, periodo+1):
+        fluxo = []
+        arq = open('incremental/fluxo_puro_' + min + '_' + str(p) + '.txt', 'r')
+        texto = arq.readlines()
+        for linha in texto:
+            a = linha.split(',')
+            fluxo.append(a)
+        arq.close()
+
+
+        label = []
+
+        for i in fluxo:
+            c = 0
+
+            if int(i[1]) == 14444:
+                j = 1.0
+                c = 1
+            elif int(i[1]) == 2222:
+                j = 1.0
+                c = 1
+            elif int(i[1]) == 3032:
+                j = 1.0
+                c = 1
+            elif int(i[1]) == 8005:
+                j = 1.0
+                c = 1
+
+            if c != 1:
+                if int(i[3]) == 14444:
+                    j = 1.0
+                elif int(i[3]) == 2222:
+                    j = 1.0
+                elif int(i[3]) == 3032:
+                    j = 1.0
+                elif int(i[3]) == 8005:
+                    j = 1.0
+                else:
+                    j = 0.0
+            label.append(j)
+
+        for i in label:
+            with open('incremental/lbl_fluxos_' + min + '_' + str(p) + '.txt', 'a') as arq:
+                arq.write(str(i))
+                arq.write('\n')
+
+get_label('0min', 10)
+get_label('5min', 10)
+get_label('10min', 10)
+get_label('15min', 10)
+get_label('20min', 10)
+get_label('25min', 10)
+get_label('30min', 10)
--- Gráficos/resultados_incremental_24h.py
+++ Gráficos/resultados_incremental_24h.py
@@ -4,10 +4,13 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
 
-periodo = 2
+periodo = 10
 desejada, prevista, acuracia = dict(), dict(), dict()
 tp, fp, fn, tn, tpr, fpr = dict(), dict(), dict(), dict(), dict(), dict()
 precision, sensibilidade, especificidade, acuracia = dict(), dict(), dict(), dict()
+
+def calc_media(lista):
+    return sum(lista) / len(lista)
 
 
 def get_rate(tpo, tne, fpo, fne):
@@ -29,17 +32,45 @@ for per in range(1, periodo+1):
         precision[str(i)+'_'+str(per)] = (tp[str(i)+'_'+str(per)] / (tp[str(i)+'_'+str(per)] + fp[str(i)+'_'+str(per)])) * 100
         sensibilidade[str(i)+'_'+str(per)] = (tp[str(i)+'_'+str(per)] / (tp[str(i)+'_'+str(per)] + fn[str(i)+'_'+str(per)])) * 100
         especificidade[str(i)+'_'+str(per)] = (tn[str(i)+'_'+str(per)] / (tn[str(i)+'_'+str(per)] + fp[str(i)+'_'+str(per)])) * 100
-        acuracia[str(i)+'_'+str(per)] = (accuracy_score(desejada[str(i)+'_'+str(per)], prevista[str(i)+'_'+str(per)])) * 100
 
 fig, ax = plt.subplots()
+'''
+x = ['00:00', '00:05', '00:10', '00:15', '00:20', '00:25', '00:30', '00:35', '00:40', '00:45', '00:50', '00:55',
+'01:00', '01:05', '01:10', '01:15', '01:20', '01:25', '01:30', '01:35', '01:40', '01:45', '01:50', '01:55',
+'02:00', '02:05', '02:10', '02:15', '02:20', '02:25', '02:30', '02:35', '02:40', '02:45', '02:50', '02:55',
+'03:00', '03:05', '03:10', '03:15', '03:20', '03:25', '03:30', '03:35', '03:40', '03:45', '03:50', '03:55',
+'04:00', '04:05', '04:10', '04:15', '04:20', '04:25', '04:30', '04:35', '04:40', '04:45', '04:50', '04:55',
+'05:00', '05:05', '05:10', '05:15', '05:20', '05:25', '05:30', '05:35', '05:40', '05:45']
+'''
+precision_l = [precision['0_1']]
+sensibilidade_l = [sensibilidade['0_1']]
+especificidade_l = [especificidade['0_1']]
+acuracia_l= [acuracia['0_1']]
 
-x = ['00:00', '00:05', '00:10', '00:15', '00:20', '00:25', '00:30',
-     '00:35', '00:40', '00:45', '00:50', '00:55', '01:00', '01:05']
+for i in range(1, periodo+1):
+    precision_l.append(calc_media([precision[str(5) + '_' + str(i)], precision[str(10) + '_' + str(i)],
+                          precision[str(15) + '_' + str(i)], precision[str(20) + '_' + str(i)],
+                          precision[str(25) + '_' + str(i)], precision[str(30) + '_' + str(i)]]))
+    sensibilidade_l.append(calc_media([sensibilidade[str(5) + '_' + str(i)], sensibilidade[str(10) + '_' + str(i)],
+                                       sensibilidade[str(15) + '_' + str(i)], sensibilidade[str(20) + '_' + str(i)],
+                                       sensibilidade[str(25) + '_' + str(i)], sensibilidade[str(30) + '_' + str(i)]]))
+    especificidade_l.append(calc_media([especificidade[str(5) + '_' + str(i)], especificidade[str(10) + '_' + str(i)],
+                                        especificidade[str(15) + '_' + str(i)], especificidade[str(20) + '_' + str(i)],
+                                        especificidade[str(25) + '_' + str(i)], especificidade[str(30) + '_' + str(i)]]))
+    acuracia_l.append(calc_media([acuracia[str(5) + '_' + str(i)], acuracia[str(10) + '_' + str(i)],
+                                  acuracia[str(15) + '_' + str(i)], acuracia[str(20) + '_' + str(i)],
+                                  acuracia[str(25) + '_' + str(i)], acuracia[str(30) + '_' + str(i)]]))
 
-ax.plot(x, list(precision.values()), color='red', label='Precision', marker='o')
-ax.plot(x, list(sensibilidade.values()), color='yellow', label='Sensibility', marker='x')
-ax.plot(x, list(especificidade.values()), color='blue', label='Specificity', marker='v')
-ax.plot(x, list(acuracia.values()), color='green', label='Acuracy', marker='*')
+
+
+
+x = ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00',
+     '04:30', '05:00']
+ax.plot(x, precision_l, color='red', label='Precision', marker='o')
+ax.plot(x, sensibilidade_l, color='yellow', label='Sensibility', marker='x')
+ax.plot(x, especificidade_l, color='blue', label='Specificity', marker='v')
+ax.plot(x, acuracia_l, color='green', label='Acuracy', marker='*')
+#ax.plot(x, list(acuracia.values()), color='green', label='Acuracy', marker='*')
 
 # Tamanho dos Ticks
 plt.yticks(fontsize=16)
